26bd29b05495748872f444fb3e32a7a68aed7c47
Cannonbait <ivar_busig@hotmail.com>
2016-02-13 15:01:19 +0100

    Working but still to slow

--- Problem529/main.py
+++ Problem529/main.py
@@ -12,13 +12,13 @@ def sum_10(str_num):
     sum += int(num)
   return sum
 
-def complete_sub(num, num_set):
-  return set(range(0, len(str(num)))) == num_set
+def complete_sub(num):
+  return set(range(0, len(str(num)))) == hash_num10[num]
 
 def substring_10(num):
   str_num = str(num)
   if num in hash_num10:
-    return complete_sub(num, hash_num10[num])
+    return complete_sub(num)
   prenum = int(str_num[:-1])
   index = len(str_num)
   sum = 0
@@ -27,9 +27,9 @@ def substring_10(num):
     sum += int(str_num[index])
   if sum == 10:
     num_set = set(range(index, len(str_num)))
-    num_set.union(hash_num10[prenum])
+    num_set = num_set.union(hash_num10[prenum])
     hash_num10[num] = num_set
-    return complete_sub(num, num_set)
+    return complete_sub(num)
   else:
     hash_num10[num] = hash_num10[prenum]
     return False
@@ -42,7 +42,7 @@ def count_num_substr_10(exponent):
   return count
 
 seed_hash_num10()
+print(count_num_substr_10(6))
 #print(hash_num10)
-print(count_num_substr_10(2))
-print(count_num_substr_10(5))
+#print(count_num_substr_10(5))
 #print(is_substr_10(190))
